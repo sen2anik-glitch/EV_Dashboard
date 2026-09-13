f48093980b0e079a63cd97c6afcb90ee7c87e453
sen2anik-glitch <sen2puku@gmail.com>
2026-09-14 01:49:38 +0530

    etl_pipeline.py

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -2,14 +2,14 @@ import requests
 import pandas as pd
 
 OUTPUT_FILE = "cleaned_ev_stations.csv"
-BASE_URL = "https://data.ny.gov/resource/bpkw-vkpj.json"
+BASE_URL = "https://data.wa.gov/resource/f6w7-q2d2.json"
 
-print("Fetching data from New York State Open Data...")
-response = requests.get(f"{BASE_URL}?$limit=500")
+print("Fetching data from Washington State Gov Open Data...")
+response = requests.get(f"{BASE_URL}?$limit=1000")
 
 print(f"Server response code: {response.status_code}")
 data = response.json()
-print(f"Successfully downloaded {len(data)} stations.")
+print(f"Successfully downloaded {len(data)} records.")
 
 # Convert the raw data straight into a spreadsheet and save it
 df = pd.DataFrame(data)
